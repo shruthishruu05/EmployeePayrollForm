8a6520c8233305c5d98856c3ecbe6f1b725e9ca2
shruthishruu05 <shruthi.m@perfios.com>
2021-10-12 00:05:22 +0530

    ADDED : Validation for name and start date

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,71 +1,25 @@
-let employeeArr = [];
+var salaryOutput = document.querySelector(".salary-output");
+var salary = document.querySelector("#salary");
+salary.addEventListener("input", function () {
+  salaryOutput.textContent = salary.value;
+});
 
-class EmployeePayrollData {
-        
-    //constructor
-    constructor(...params) {
-        this.name = params[0];
-        this.salary = params[1];
-        this.gender = params[2];
-        this.startDate = params[3];
-        this.departments = params[4];
-        this.notes = params[5];
-    }
-    //getter and setter
-    get name() { return this._name; 
-    }
-    set name(name) {
-        this._name = name;
-    }
-    get salary() { return this._salary; }
-    set salary(salary) {
-        this._salary = salary;
-    }
-    get gender() { return this._gender; }
-    set gender(gender) {
-        this._gender = gender;
-    }
-    get startDate() { return this._startDate; }
-    set startDate(startDate) {
-        this._startDate = startDate; 
-    }
-    get departments() { return this._departments}
-    set departments(departments){
-        this._departments = departments;
-    }
-    get notes() { return notes}
-    set notes(notes) {
-        this._notes = notes;
-    }
+function save(e) {
+  let name = document.getElementById("name").value;
+  let date = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let startDate = Date.parse(year + "-" + month + "-" + date);
 
-    //method
-    toString() {
-        return "id="+ this.id +", name="+ this.name +", salary="+ this.salary+", gender="+ this.gender+", start date="+ this.startDate+", department="+this.departments;
-    }
-}
+  let nameRegex = RegExp("^[A-Z][a-z]{2,}$");
+  if (!nameRegex.test(name)) {
+    alert("Invalid name");
+    return false;
+  }
 
-function save(){
-    const name = document.querySelector("#name");
-    let departments = new Array();
-    let markedCheckbox = document.getElementsByName('department');  
-    for (var checkbox of markedCheckbox) {  
-      if (checkbox.checked)  
-        departments.push(checkbox.value);  
-    } 
-    let markedRadioButton = document.getElementsByName('gender');
-    let gender;
-    for (var radiobutton of markedRadioButton) {  
-        if (radiobutton.checked)  
-          gender = radiobutton.value;  
-    }
-    const salary = document.querySelector('#salary');
-    const day = document.querySelector('#day');
-    const month = document.querySelector('#month');
-    const year = document.querySelector('#year');
-    const date = new Date(day.value+"/"+month.value+"/"+year.value);
-    const notes = document.querySelector('#notes'); 
-
-    employeeArr.push(new EmployeePayrollData(name.value,salary.value,gender,date,departments,notes.value));
-
-    console.log(employeeArr);
+  let maxDateGap = 30 * 24 * 60 * 60 * 1000;
+  if (startDate - Date.now() > maxDateGap) {
+    alert("Invalid Date");
+    return false;
+  }
 }
