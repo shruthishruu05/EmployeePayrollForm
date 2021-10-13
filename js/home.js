0bf9b3cf9a51fc3e4527973e625d3cd8c27d1cbf
shruthishruu05 <shruthi.m@perfios.com>
2021-10-13 20:50:40 +0530

    ADDED: code to view employee in tabular form using template literals

---
+++ js/home.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+    const innerHtml = `${headerHtml}
+
+    <tr>
+        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
+        <td>Narayan Mahadevan</td>
+        <td>Male</td>
+        <td>
+            <div class="dept-label">HR</div>
+            <div class="dept-label">Finance</div>
+        </td>
+        <td>300000</td>
+        <td>24 APR 2020</td>
+        <td>
+            <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+        </td>
+    </tr>
+    `;
+    document.querySelector('#display').innerHTML = innerHtml;
+}
