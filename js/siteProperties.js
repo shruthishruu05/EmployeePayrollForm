83c9b8094af96172732aa3e5da59f9dcf3f6a8d5
shruthishruu05 <shruthi.m@perfios.com>
2021-10-18 21:47:51 +0530

    ADDED : new site properties

--- js/siteProperties.js
+++ js/siteProperties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "../Pages/home.html",
-    add_emp_payroll_page: "../Pages/EmployeePayrollForm.html"
+    add_emp_payroll_page: "../Pages/EmployeePayrollForm.html",
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
 };
